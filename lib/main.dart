812bdcb4f9720e50fbb905a385ac2a723234cb27
tika kartika <tikaputri469@gmail.com>
2026-02-22 13:35:39 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pratikum_dua_kelompok_tiga/camera.dart' show CameraExampleHome;
+import 'package:pratikum_dua_kelompok_tiga/connectivity_plus.dart'
+    show ConnectivityPage;
+import 'package:pratikum_dua_kelompok_tiga/geolocator.dart'
+    show GeolocatorWidget;
+import 'package:pratikum_dua_kelompok_tiga/image_picker.dart' show MyAppImage;
+import 'package:pratikum_dua_kelompok_tiga/mobile_scanner.dart' show MobileScannerSimple;
+
 
 void main() {
   runApp(const MyApp());
@@ -7,24 +15,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Praktikum Plugin Flutter',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Menu Praktikum Flutter'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  
 
   final String title;
 
@@ -33,45 +37,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
+  Widget menuButton(String title, Widget page) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 50),
+        ),
+        child: Text(title),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => page),
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    
     return Scaffold(
       appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
         child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            menuButton("Camera", const CameraExampleHome()),
+
+            menuButton("Connectivity Plus", const ConnectivityPage()),
+
+            menuButton("Geolocator", const GeolocatorWidget()),
+
+            menuButton("Image Picker", const MyAppImage()),
+
+            menuButton("Mobile_Scanner", const MobileScannerSimple()),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
