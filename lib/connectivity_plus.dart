7df0efae51b4b5e0d9495d8312debc485ac63b0f
tika kartika <tikaputri469@gmail.com>
2026-02-22 13:35:09 +0700

    Update connectivity_plus.dart

--- lib/connectivity_plus.dart
+++ lib/connectivity_plus.dart
@@ -11,12 +11,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ConnectivityPage extends StatelessWidget {
+  const ConnectivityPage({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
